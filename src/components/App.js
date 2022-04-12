865c9ae164d98f0a8b91e2ec09561332b51f54c2
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-04-12 11:34:56 -0600

    Install react router and organize files into correct folders.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../css/App.css';
+
+const App = () => {
+  return (
+    <div className='app-div'>
+      <h1>Top Stories</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import '../css/Main.css';
+import StoryContainer from './StoryContainer';
+
+const Main = () => {
+  
+
+  return (
+    <div>
+      <header>Top Stories</header>
+      <section className='top-stories-section'>
+        
+      </section>
+    </div>
+  )
+}
+
+export default Main;
